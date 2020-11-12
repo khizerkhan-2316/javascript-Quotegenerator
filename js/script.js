8858251db6510a589bdc2d9d1292d87837281383
Khizer Khan <khizer759@gmail.com>
2020-11-13 00:42:46 +0100

    Added JS functionality to the quote generator

--- js/script.js
+++ js/script.js
@@ -4,20 +4,102 @@ const quotes = [
   {
     quote: "If you want something done right, do it yourself.",
     author: "Herman Bang",
-    tags: ["productivity", "quality", "you"],
+    tags: ["you", "quality"],
+  },
+
+  {
+    quote:
+      "If you want something said, ask a man; if you want something done, ask a woman.",
+    author: "Hvid Steen",
+    tags: ["quality ", "persons"],
+  },
+
+  {
+    quote:
+      "What really knocks me out is a book that, when you're all done reading it, you wish the author that wrote it was a terrific friend of yours and you could call him up on the phone whenever you felt like it. That doesn't happen much, though.",
+    author: "J.D. Salinger",
+    tags: ["authors", "books", "literature", "reading"],
   },
 ];
 
-/*const createParagraph = (target, source) => {
-  let paragraph = document.createElement(source);
+const insertQuote = () => {
+  const quote = document.getElementById("quoteInput").value;
+  const author = document.getElementById("authorInput").value;
+  const tags = document.getElementById("tagsInput").value;
+  let error = document.getElementById("errorMessage");
 
-  let tags = quotes.forEach((element) => {
-    paragraph.innerHTML = element.tags[0];
-  });
+  if (quote == null || quote === "") {
+    error.innerHTML = "Enter a valid quote input!";
+  } else if (author == null || author === "") {
+    error.innerHTML = "Enter a valid author input";
+  } else if (tags == null || tags === "") {
+    error.innerHTML = "Enter a valid tag input";
+  }
 
-  document.getElementById(target).appendChild(paragraph);
+  if (
+    quote === quote &&
+    author === author &&
+    tags === tags &&
+    quote != null &&
+    quote != "" &&
+    author != null &&
+    author != "" &&
+    tags != null &&
+    tags != ""
+  ) {
+    let object = {
+      quote: quote,
+      author: author,
+      tags: [tags],
+    };
+
+    quotes.push(object);
+
+    error.innerHTML = "Your quote is added! - See it in the quotes";
+  }
+
+  /* Skal tage værdien/teksten som bliver skrevet i textarea og de 2 tekstfelter
+ i html og retunere et object og pushe det 
+i quotes array af objekt-samling. 
+*/
 };
 
-document.getElementById("body").onload = function () {
-  createParagraph("tag", "P");
-};*/
+const execute = () => {
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+  document.getElementById("create").style.display = "none";
+  document.getElementById("quote").innerHTML = `"${randomQuote.quote}"`;
+  document.getElementById("author").innerHTML = `- ${randomQuote.author}`;
+
+  document.getElementById("tag").innerHTML = "";
+
+  randomQuote.tags.forEach((element) => {
+    let span = document.createElement("SPAN");
+    span.innerHTML = `#${element}`;
+
+    document.getElementById("tag").appendChild(span);
+  });
+};
+
+const showElement = (element, visabillity) => {
+  document.getElementById(element).style.display = visabillity;
+};
+
+const hideElement = (element) => {
+  document.getElementById(element).style.display = "none";
+};
+
+// onclick functions:
+document.getElementById("autogenerate").onclick = function () {
+  execute();
+};
+
+document.getElementById("create").onclick = function () {
+  hideElement("main-frame-js"), showElement("create-quote-container", "block");
+};
+
+document.getElementById("submit").onclick = function () {
+  insertQuote(),
+    hideElement("create-quote-container"),
+    showElement("main-frame-js", "block");
+};
