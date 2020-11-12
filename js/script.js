43897fc95b888fb5c2615f58a17d537c60144c47
Khizer Khan <khizer759@gmail.com>
2020-11-12 19:34:48 +0100

    Updated html, css and js files

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 console.log("Javascript up and running!");
+
+const quotes = [
+  {
+    quote: "If you want something done right, do it yourself.",
+    author: "Herman Bang",
+    tags: ["productivity", "quality", "you"],
+  },
+];
+
+/*const createParagraph = (target, source) => {
+  let paragraph = document.createElement(source);
+
+  let tags = quotes.forEach((element) => {
+    paragraph.innerHTML = element.tags[0];
+  });
+
+  document.getElementById(target).appendChild(paragraph);
+};
+
+document.getElementById("body").onload = function () {
+  createParagraph("tag", "P");
+};*/
